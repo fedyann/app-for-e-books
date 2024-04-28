859963ca7f10192bd4a84b9b2d670346e9b80b0f
fedyann <117765096+fedyann@users.noreply.github.com>
2024-04-29 00:42:44 +0300

    Delete source directory

--- source/script/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-  $('.header__burger,.header__list').click(function (event) {
-    $('.header__burger,.header__menu').toggleClass('active');
-    $('body').toggleClass('lock');
-  });
-  $('.clear').click(function (event) {
-    $('body').toggleClass('lock');
-  });
-});
