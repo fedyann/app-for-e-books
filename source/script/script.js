b09c082d89b4cb4e8d6e1abfef304d4bbb5c90a2
fedyann <117765096+fedyann@users.noreply.github.com>
2024-04-29 00:46:35 +0300

    Add files via upload

---
+++ source/script/script.js
@@ -0,0 +1,11 @@
+document.createElement('figure');
+document.createElement('figcaption');
+$(document).ready(function () {
+  $('.header__burger,.header__list').click(function (event) {
+    $('.header__burger,.header__menu').toggleClass('active');
+    $('body').toggleClass('lock');
+  });
+  $('.clear').click(function (event) {
+    $('body').toggleClass('lock');
+  });
+});
