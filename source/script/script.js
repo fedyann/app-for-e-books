895da94439a7138821f7740dff6e299574705e61
fedyann <117765096+fedyann@users.noreply.github.com>
2024-05-02 00:32:42 +0300

    Update script.js

--- source/script/script.js
+++ source/script/script.js
@@ -1,5 +1,8 @@
 document.createElement('figure');
 document.createElement('figcaption');
+document.addEventListener ('selectionchange', function (){
+  // getSelection().toString();
+}, false);
 $(document).ready(function () {
   $('.header__burger,.header__list').click(function (event) {
     $('.header__burger,.header__menu').toggleClass('active');
@@ -8,4 +11,30 @@ $(document).ready(function () {
   $('.clear').click(function (event) {
     $('body').toggleClass('lock');
   });
+});
+
+function styliString(color) {
+  const selection = getSelection();
+  const range = selection.getRangeAt(0);
+  const selectionContents = range.extractContents();
+  const span = document.createElement("span");
+  
+  for(const oldSpan of selectionContents.querySelectorAll(".selected")) {
+    for(const childNode of [...oldSpan.childNodes])
+      oldSpan.parentNode.insertBefore(childNode, oldSpan);
+    
+    oldSpan.remove();
+  }
+  
+  selectionContents.normalize();
+  span.classList.add("selected");
+  span.style.backgroundColor = color;
+  span.appendChild(selectionContents);
+  range.insertNode(span);
+}
+
+document.addEventListener("click", ({ target }) => {
+  if(target.classList.contains("color")) {
+    styliString(target.dataset.color || target.textContent);
+  }
 });
